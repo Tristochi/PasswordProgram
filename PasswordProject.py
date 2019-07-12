b5cf0d4eb84bb160f87ffbb1b2b0e281a9d1cfd7
Tristochi <TristanLSzakacs@gmail.com>
2019-07-12 12:28:25 -0400

    -Created a New Account button with functionality. Opens a Toplevel with entry fields and two buttons of its own. -Added functionality to cancel button from Toplevel window.

--- PasswordProject.py
+++ PasswordProject.py
@@ -14,22 +14,49 @@ class MyApp(Frame):
 		self.master = master
 		self.pack()
 		self.create_widgets()
+		self.master.title("Password Book")
+		
 		
 	def create_widgets(self):
 		self.accounts_label = Label(self, text="Accounts: ", fg = "black")
 		self.accounts_label.pack()
 		self.account_window = Listbox(self)
-		self.account_window.pack()
-		self.account_window.insert(END, "A list entry")
+		self.account_window.pack(padx = 5, pady = 5, side = LEFT)
+		# self.account_window.insert(END, "A list entry")3
 		
-	def say_hi(self):
-		print("Hi there, everyone!")
+		self.add = Button(self, text="New Account", fg= "black", command=self.add_new)
+		self.add.pack(padx = 5, pady = 5, side = LEFT)
+
+
+	def add_new(self):
+		new_pw = Toplevel()
+		new_pw.title("Add Account: ")
+		
+		# Label and entry for Username
+		Label(new_pw, text="Username: ", fg="black").grid(row=0, padx = 5, pady = 10)
+		Entry(new_pw).grid(row=0, column=1, padx = 10, pady = 10)
+		
+		# Label and entry for Password
+		Label(new_pw, text="Password: ", fg="black").grid(row = 1, padx = 5, pady = 10)
+		Entry(new_pw).grid(row = 1, column = 1, padx = 10, pady = 10)
+		
+		# Add button
+		Button(new_pw, text="Apply", fg="black", command=self.update).grid(row = 2, pady = 20)
+		
+		# Cancel button
+		Button(new_pw, text="Cancel", fg="black", command=lambda:new_pw.destroy()).grid(row=2, column=1, padx = 10, pady = 20)
+	
+	
+	def update(self):
+		pass
+
+	
+		
 
 if __name__ == '__main__':
 	root = Tk()
 	app = MyApp(master=root)
 	
-	for item in ["one", "two", "three", "four"]:
-		app.account_window.insert(END, item)
+	
 	app.mainloop()
 		
