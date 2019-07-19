dcacdc761c9f7c73c131af65524f10ec85ecb5ec
Tristochi <TristanLSzakacs@gmail.com>
2019-07-19 15:06:35 -0400

    Implemented SQLite DB. Still working on populating Listbox properly.

--- PasswordProject.py
+++ PasswordProject.py
@@ -1,5 +1,10 @@
 from tkinter import *
 import sys
+import sqlite3
+
+accounts = []
+users = []
+passwords = []
 
 """ This program when opened will prompt a user to create a login. After creating a login, the user will be asked to log in.
 When the user enters the correct information, they will see the password book. It will have a widget that shows all account
@@ -15,18 +20,16 @@ class MyApp(Frame):
 		self.pack()
 		self.create_widgets()
 		self.master.title("Password Book")
-		self.account_list = {"account": [], "username": [], "password": []}
-		self.accounts = []
-		self.users = []
-		self.passwords = []
+		self.refresh()
 		
 		
 	def create_widgets(self):
 		self.accounts_label = Label(self, text="Accounts: ", fg = "black")
 		self.accounts_label.pack()
-		self.account_window = Listbox(self)
+		self.lb_values = Variable(value=accounts)
+		
+		self.account_window = Listbox(self, listvariable = self.lb_values)
 		self.account_window.pack(padx = 5, pady = 5, side = LEFT)
-		# self.account_window.insert(END, "A list entry")3
 		
 		self.add = Button(self, text="New Account", fg= "black", command=self.add_new)
 		self.add.pack(padx = 5, pady = 5, side = LEFT)
@@ -40,16 +43,19 @@ class MyApp(Frame):
 		global pw
 		# Label and entry for account title
 		Label(new_pw, text="Account Title: ", fg = "black").grid(row=0, padx=5, pady =10)
+		global title
 		title = Entry(new_pw)
 		title.grid(row=0, column=1, padx=10, pady=10)
 	
 		# Label and entry for Username
 		Label(new_pw, text="Username: ", fg="black").grid(row=1, padx = 5, pady = 10)
+		global user
 		user = Entry(new_pw)
 		user.grid(row=1, column=1, padx = 10, pady = 10)
 		
 		# Label and entry for Password
 		Label(new_pw, text="Password: ", fg="black").grid(row = 2, padx = 5, pady = 10)
+		global pw
 		pw = Entry(new_pw)
 		pw.grid(row = 2, column = 1, padx = 10, pady = 10)
 		
@@ -61,25 +67,43 @@ class MyApp(Frame):
 	
 	
 	def update(self, master):
-		self.accounts.append(title.get())
-		print(self.accounts)
-		self.users.append(user.get())
-		print(self.users)
-		self.passwords.append(pw.get())
-		print(self.passwords)
 
+		c.execute('CREATE TABLE IF NOT EXISTS passwords(accountName TEXT, username TEXT, password TEXT)')
 		
-		for values in range(len(self.accounts)):
-			self.account_list["account"].append(self.accounts[values])
-			self.account_window.insert(END, self.accounts[values])
+		acc_input = title.get()
+		usr_input = user.get()
+		pass_input = pw.get()
+		
+		accounts.append(acc_input)
+		users.append(usr_input)
+		passwords.append(pass_input)
+		
+		acc_index = len(accounts) - 1
+		user_index = len(users) - 1
+		pass_index = len(passwords) - 1
+		
+		c.execute('INSERT INTO passwords(accountName, username, password) VALUES(?, ?, ?)', (accounts[acc_index], users[user_index], passwords[pass_index]))
+		conn.commit()
+		
+		self.account_window.insert(END, acc_input)
 		
 		master.destroy()				
 		
 		
+	def refresh(self):
+		 c.execute("SELECT accountName FROM passwords")
+		 
+		 for tup in c.fetchall():
+			for usr in tup:
+				self.account_window.insert(END, usr)
+
 
 if __name__ == '__main__':
+	conn = sqlite3.connect('Pbook.db')
+	c = conn.cursor()
 	root = Tk()
 	app = MyApp(master=root)
+
 	
 	
 	app.mainloop()
